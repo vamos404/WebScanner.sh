c145d29ec5b7edddef57a24833a55a6d02c203c4
vamos404 <75011852+vamos404@users.noreply.github.com>
2024-10-30 14:10:05 -0400

    Add files via upload

---
+++ WebScanner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo
+echo -e "Select scanning tool: \n1= nmap\n2= nikto\n3= dirb\n4= enum4linux"
+read sel
+echo
+# 1st choice: nmap scan
+if [[ $sel -eq 1 ]];
+then
+	echo "Enter the target IP:  "
+	read IP
+	nmap -p- -sS -sV -T4 -A -O $IP
+	
+elif [[ $sel -eq 2 ]];
+then
+	echo "Enter target IP: "
+	read IP
+	nikto -h $IP
+
+# 3rd choice: dirb
+elif [[ $sel -eq 3 ]];
+then 
+	echo "Enter the target IP: "
+	read IP
+	dirb http://$IP
+
+# 4th choice: enum4linux
+elif [[ $sel -eq 4 ]];
+then 
+	echo "Enter the target IP: "
+	read IP
+	enum4linux $IP
+
+exit 0
+fi
