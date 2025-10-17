7f0be3965a874e801c859f286922fddef7ac05e0
Nazir_404 <vamosalbiselesta@gmail.com>
2025-10-17 11:13:36 +0600

    Messi is the GOAT

--- WebScanner.sh
+++ WebScanner.sh
@@ -3,6 +3,7 @@
 # Description: This will help to scann any Web Application within short time.
 # Date creation: 31/10/2024
 # Last Modified: 31/10/2024
+# Messi is the GOAT
 
 echo
 echo -e "Select scanning tool: \n1= nmap\n2= nikto\n3= dirb\n4= enum4linux"
